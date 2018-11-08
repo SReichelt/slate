24154feb2dc0027aee4bde35d45b969db5f443bc
Sebastian Reichelt <SebastianR@gmx.de>
2018-11-08 01:16:53 +0100

    Added code for mail submission.

--- src/server/routes/api-router.ts
+++ src/server/routes/api-router.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as express from 'express';
 import { Router } from 'express';
+import * as nodemailer from 'nodemailer';
 import * as config from '../config';
 
 export function apiRouter() {
@@ -14,12 +15,37 @@ export function apiRouter() {
 
   router.use('/fonts', express.static(fontPath));
 
+  let mailTransporter = config.MAIL_TRANSPORT_CONFIG ? nodemailer.createTransport(config.MAIL_TRANSPORT_CONFIG) : undefined;
+
   router.put('/libraries/*', (request, response) => {
+    let requestPath = decodeURI(request.url);
     if (config.IS_PRODUCTION) {
-      // TODO
-      response.sendStatus(501);
+      if (mailTransporter && config.MAIL_FROM && config.MAIL_TO) {
+        let mail: any = {
+          from: config.MAIL_FROM,
+          to: config.MAIL_TO,
+          subject: 'HLM submission: ' + requestPath,
+          text: requestPath,
+          attachments: [{
+            filename: path.basename(requestPath),
+            contentType: 'text/plain',
+            contentTransferEncoding: 'quoted-printable',
+            content: request
+          }]
+        };
+        mailTransporter.sendMail(mail, (error: any) => {
+          if (error) {
+            console.log(error);
+            response.sendStatus(503);
+          } else {
+            response.sendStatus(202);
+          }
+        });
+      } else {
+        response.sendStatus(501);
+      }
     } else {
-      let fileName = path.join(dataPath, decodeURI(request.url));
+      let fileName = path.join(dataPath, requestPath);
       request.pipe(fs.createWriteStream(fileName));
       request.on('end', () => response.sendStatus(200));
     }
