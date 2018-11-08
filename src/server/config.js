f5450c065c813bc112277e1fc1e33b1d0ccd071a
Sebastian Reichelt <SebastianR@gmx.de>
2018-11-08 01:24:06 +0100

    Added environment variables for mail config.

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,15 @@
 module.exports = {
   IS_PRODUCTION: process.env.NODE_ENV === 'production',
-  SERVER_PORT: process.env.PORT || 3000
+  SERVER_PORT: process.env.PORT || 3000,
+  MAIL_TRANSPORT_CONFIG: process.env.MAIL_HOST ? {
+    host: process.env.MAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD
+    }
+  } : undefined,
+  MAIL_FROM: process.env.MAIL_FROM,
+  MAIL_TO: process.env.MAIL_TO
 };
