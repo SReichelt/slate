f6e732aefc282f47c09d914f3e01448b6c59c95c
Sebastian Reichelt <SebastianR@gmx.de>
2019-02-17 13:08:03 +0100

    Added library as submodule.

--- src/scripts/cloneLibraries.ts
+++ src/scripts/cloneLibraries.ts
@@ -3,6 +3,5 @@ import { spawnSync } from 'child_process';
 const Libraries = require('../../data/libraries/libraries.json');
 
 for (let libraryName of Object.keys(Libraries)) {
-  let library = Libraries[libraryName];
-  spawnSync('git', ['clone', '-b', library.branch, `https://github.com/${library.repository}.git`, `./data/libraries/${libraryName}`]);
+  spawnSync('git', ['submodule', 'update', `./data/libraries/${libraryName}`]);
 }
