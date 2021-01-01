d79ccea141e058d2e988fce0ce761b58bc9ec818
Tim J. Baumann <tim@timbaumann.info>
2021-01-01 13:23:40 +0100

    Add curated list of initial LaTeX autocomplete suggestions

--- src/client/utils/latexInput.ts
+++ src/client/utils/latexInput.ts
@@ -12,7 +12,32 @@ const allReplacements: LatexInputReplacement[] =
     .map(([latexCode, unicodeCharacters]) => ({ latexCode, unicodeCharacters }))
     .sort((a, b) => a.latexCode.localeCompare(b.latexCode, 'en'));
 
+const findReplacementByLatexCode = (latexCode: string): LatexInputReplacement => {
+  const replacement = allReplacements.find(r => r.latexCode === latexCode);
+  if (replacement === undefined) {
+    throw new Error(`Could not find replacement with LaTeX code ${latexCode}`);
+  }
+  return replacement;
+};
+
+// A curated list of suggestions to display when the user types a single backslash
+const initialSuggestions: LatexInputReplacement[] = [
+  '\\~{a}',
+  '\\={a}',
+  '\\mathbb{i}',
+  '\\mathcal{M}',
+  '\\v{C}',
+  '\\alpha',
+  '\\aleph',
+  '\\hbar',
+  '\\simeq',
+  '\\otimes'
+].map(findReplacementByLatexCode);
+
 export function getLatexInputSuggestions(current: string): LatexInputReplacement[] {
+  if (current === '\\') {
+    return initialSuggestions;
+  }
   return allReplacements
     .filter(({ latexCode }) => latexCode.startsWith(current))
     .slice(0, MAX_SUGGESTIONS);
